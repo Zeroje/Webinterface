a2a48615cea766f6b4ba4dafc96105e9a48b5f0e
Zeroje <34905555+Zeroje@users.noreply.github.com>
2024-08-16 03:04:10 -0400

    Update constants.ts
    Signed-off-by: Zeroje <34905555+Zeroje@users.noreply.github.com>

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_PATH = import.meta.env.VITE_API_URL || "https://api.ree6.de"
-export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://invite.ree6.de"
+export const BASE_PATH = import.meta.env.VITE_API_URL || "http://thedeathstar.zapto.org"
+export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://discord.com/oauth2/authorize?client_id=1258839818138550414&permissions=8&integration_type=0&scope=bot"
 
 export function get(path: string) {
 
